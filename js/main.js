871b3bed40406c57b933cdfcc6ccca45e831bc67
Ayodele Owoyele <test@w3schools.com>
2023-04-15 08:59:09 -0500

    create javascript folder, removed javascript script tag from html and moved it to main.js file

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    // Code to be executed when the document finishes loading
+    var dt = new Date();
+    document.getElementById('copyr').innerHTML = "Copyright " + dt.getFullYear() + "(c)";
+    console.log("Document loaded!");
+};
+  
+
